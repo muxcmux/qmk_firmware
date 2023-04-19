f4c1e824922f69e0a9ae9deb3dc65ff166a6b5bc
muxcmux <storm.bg@gmail.com>
2023-04-19 10:19:20 +0100

    Add my own layout for the corne keyb

---
+++ keyboards/crkbd/keymaps/muxcmux/rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+MAGIC_ENABLE    = yes
+CONSOLE_ENABLE  = no
+NKRO_ENABLE     = yes
+
