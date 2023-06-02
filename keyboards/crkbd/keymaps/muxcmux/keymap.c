76908e93d192a7a96c135fb54d0dffb25f7f1a87
muxcmux <storm.bg@gmail.com>
2023-06-02 23:28:37 +0100

    Swap layer order and optimise a bit

--- keyboards/crkbd/keymaps/muxcmux/keymap.c
+++ keyboards/crkbd/keymaps/muxcmux/keymap.c
@@ -21,44 +21,46 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Macros
 enum custom_keycodes {
-    M_DSWITCH = SAFE_RANGE,
-    M_SCRSH,
+    M_EMOJI = SAFE_RANGE,
+    M_LUNCH, M_SCRSH,
+    M_MSMLE, M_MMDLE, M_MLGLE,
+    M_MSMRI, M_MMDRI, M_MLGRI,
+    M_MFULL, M_MCNTR, M_MSMCE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   KC_ESC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        MO(3),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,   MO(2),
+        MO(2),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,   MO(1),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       SC_LSPO,LCTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, RCTL_T(KC_SLSH), SC_RSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,LGUI_T(KC_SPC),KC_BSPC, KC_ENT,   MO(1), KC_RALT
+                                         KC_LGUI,LGUI_T(KC_SPC),KC_BSPC, KC_ENT,   MO(3), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ESC,
+      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, M_EMOJI, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    KC_6,    KC_7,    KC_8,    KC_9,  KC_TAB,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, M_SCRSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MPLY, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, _______, _______,    _______, _______, _______
+                                          _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
-
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_CAPS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX, M_DSWITCH,
+      _______, RGB_TOG, RGB_VAD, RGB_VAI, RGB_MOD, XXXXXXX,                      KC_PLUS, KC_LBRC, KC_RBRC, KC_MINS, XXXXXXX, KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, RGB_HUI, RGB_SAI, XXXXXXX, XXXXXXX, RGB_VAI,                      KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, M_SCRSH, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_EQL, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUD, RGB_SAD,RGB_RMOD, RGB_MOD, RGB_VAD,                      KC_MPLY, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_GRV, KC_QUOT, KC_DQUO, KC_AMPR, KC_TILD, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -66,13 +68,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PLUS, KC_LBRC, KC_RBRC, KC_MINS, XXXXXXX,  KC_ESC,
+      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_EQL, KC_LCBR, KC_RCBR, KC_UNDS, KC_PIPE, KC_BSLS,
+      _______,    KC_6,    KC_7,    KC_8,    KC_9,  KC_TAB,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_GRV, KC_QUOT, KC_DQUO, KC_AMPR, KC_TILD, _______,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          KC_LALT, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
@@ -88,11 +90,38 @@ combo_t key_combos[COMBO_COUNT] = {
 bool handle_macros(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch(keycode) {
-            case M_DSWITCH:
-                SEND_STRING(SS_LCTL(SS_LSFT(SS_LCMD("l"))));
+            case M_EMOJI:
+                SEND_STRING(SS_RCTL(SS_LGUI(" ")));
                 return false; break;
             case M_SCRSH:
                 SEND_STRING(SS_LCMD(SS_LSFT("4")));
+                return false; break;
+            case M_LUNCH:
+                SEND_STRING(SS_LCMD(" "));
+                return false; break;
+            case M_MSMLE:
+                SEND_STRING(SS_RCTL(SS_LALT("h")));
+                return false; break;
+            case M_MMDLE:
+                SEND_STRING(SS_RCTL(SS_LALT("j")));
+                return false; break;
+            case M_MLGLE:
+                SEND_STRING(SS_RCTL(SS_LALT("s")));
+                return false; break;
+            case M_MSMRI:
+                SEND_STRING(SS_RCTL(SS_LALT("l")));
+                return false; break;
+            case M_MMDRI:
+                SEND_STRING(SS_RCTL(SS_LALT("k")));
+                return false; break;
+            case M_MLGRI:
+                SEND_STRING(SS_RCTL(SS_LALT("g")));
+                return false; break;
+            case M_MCNTR:
+                SEND_STRING(SS_RCTL(SS_LALT("c")));
+                return false; break;
+            case M_MSMCE:
+                SEND_STRING(SS_RCTL(SS_LALT("d")));
                 return false; break;
         }
     }
